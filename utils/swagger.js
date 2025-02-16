41c8103caaf4a5a8d95b4d900bce64afcc8716d4
Akshay Chauhan <83492860+chauhan-akshay123@users.noreply.github.com>
2025-02-16 20:10:44 +0530

    Update swagger.js

--- utils/swagger.js
+++ utils/swagger.js
@@ -1,27 +1,25 @@
 const swaggerJsDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
+const serverUrl = process.env.RENDER_EXTERNAL_URL || "http://localhost:5000"; 
+
 const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Online Learning Platform",
+      title: "API Documentation",
       version: "1.0.0",
-      description: "API documentation using Swagger",
+      description: "API documentation for our project",
     },
-    servers: [
-      {
-        url: "http://localhost:5000/api", 
-      },
-    ],
+    servers: [{ url: serverUrl }],
   },
   apis: ["./routes/*.js"], 
+};
 
 const swaggerSpecs = swaggerJsDoc(options);
 
 const swaggerDocs = (app) => {
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
-  console.log("Swagger Docs available at http://localhost:5000/api-docs");
 };
 
 module.exports = swaggerDocs;
